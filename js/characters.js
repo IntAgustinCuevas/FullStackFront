3da25b2e08df750e584ebb14800ef9e6bcefa1b9
Agustin <Agustin2010_c@hotmail.com>
2023-11-06 18:52:40 -0300

    Casi finalizado

---
+++ js/characters.js
@@ -0,0 +1,71 @@
+document.addEventListener('DOMContentLoaded' , () => {
+    const parametro = new URLSearchParams(window.location.search);
+    const userLogin = parametro.get('id');
+
+    const nombre1 = document.getElementById('namePersonaje1')
+    const rostro1 = document.getElementById('rostro1');
+    const cuerpo1 = document.getElementById('cuerpo1');
+    const piernas1 = document.getElementById('piernas1');
+    const pies1 = document.getElementById('pies1');
+
+    const nombre2 = document.getElementById('namePersonaje2')
+    const rostro2 = document.getElementById('rostro2');
+    const cuerpo2 = document.getElementById('cuerpo2');
+    const piernas2 = document.getElementById('piernas2');
+    const pies2 = document.getElementById('pies2');
+
+    const nombre3 = document.getElementById('namePersonaje3')
+    const rostro3 =document.getElementById('rostro3');
+    const cuerpo3 = document.getElementById('cuerpo3');
+    const piernas3 =document.getElementById('piernas3');
+    const pies3 =document.getElementById('pies3');
+
+    const nombre4 = document.getElementById('namePersonaje4')
+    const rostro4 = document.getElementById('rostro4');
+    const cuerpo4 = document.getElementById('cuerpo4');
+    const piernas4 = document.getElementById('piernas4');
+    const pies4 = document.getElementById('pies4');
+
+    const nombre5 = document.getElementById('namePersonaje5')
+    const rostro5 = document.getElementById('rostro5');
+    const cuerpo5 = document.getElementById('cuerpo5');
+    const piernas5 = document.getElementById('piernas5');
+    const pies5 = document.getElementById('pies5');
+
+
+    let url = `http://localhost:4000/characters/${userLogin}`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        nombre1.textContent = data[0].name;
+        rostro1.src = data[0].personaje;
+        cuerpo1.src = data[0].parteSuperior;
+        piernas1.src = data[0].pantalon;
+        pies1.src = data[0].calzado;
+
+        nombre2.textContent = data[1].name;
+        rostro2.src = data[1].personaje;
+        cuerpo2.src = data[1].parteSuperior;
+        piernas2.src = data[1].pantalon;
+        pies2.src = data[1].calzado;
+
+        nombre3.textContent = data[2].name;
+        rostro3.src = data[2].personaje;
+        cuerpo3.src = data[2].parteSuperior;
+        piernas3.src = data[2].pantalon;
+        pies3.src = data[2].calzado;
+
+        nombre4.textContent = data[3].name;
+        rostro4.src = data[3].personaje;
+        cuerpo4.src = data[3].parteSuperior;
+        piernas4.src = data[3].pantalon;
+        pies4.src = data[3].calzado;
+
+        nombre5.textContent = data[4].name;
+        rostro5.src = data[4].personaje;
+        cuerpo5.src = data[4].parteSuperior;
+        piernas5.src = data[4].pantalon;
+        pies5.src = data[4].calzado;
+    })
+    .catch(error => console.log(error))
+})
--- js/index.js
+++ js/index.js
@@ -2,8 +2,7 @@ document.addEventListener('DOMContentLoaded' , () => {
     const parametro = new URLSearchParams(window.location.search);
     const userLogin = parametro.get('id');
 
-    let url = `http://localhost:4000/users/${userLogin}`;
-    
+    let url = `http://localhost:4000/users/${userLogin}`; 
     fetch(url)
     .then(response => response.json())
     .then(data => {
@@ -11,4 +10,78 @@ document.addEventListener('DOMContentLoaded' , () => {
         document.getElementById('usuario').textContent = userName;
     })
     .catch(error => console.log(error)) 
+
+    const defaultCara = document.getElementById('defaultCara');
+    const defaultDorso = document.getElementById('defaultDorso');
+    const defaultPiernas = document.getElementById('defaultPiernas');
+    const defaultPies = document.getElementById('defaultPies');
+
+    const personajeBox = document.getElementById('personajes');
+    personajeBox.addEventListener('click', (event) =>{
+        if(event.target.tagName === "IMG"){
+            let imgSrc = event.target.src;
+            defaultCara.src = imgSrc;
+        }
+    })
+
+    const parteSuperiorBox = document.getElementById('parteSuperior');
+    parteSuperiorBox.addEventListener('click', (event) =>{
+        if(event.target.tagName === "IMG"){
+            let imgSrc = event.target.src;
+            defaultDorso.src = imgSrc;
+        }
+    })
+
+    const pantalonBox = document.getElementById('pantalon');
+    pantalonBox.addEventListener('click', (event) =>{
+        if(event.target.tagName === "IMG"){
+            let imgSrc = event.target.src;
+            defaultPiernas.src = imgSrc;
+        }
+    })
+
+    const calzadoBox = document.getElementById('calzado');
+    calzadoBox.addEventListener('click', (event) =>{
+        if(event.target.tagName === "IMG"){
+            let imgSrc = event.target.src;
+            defaultPies.src = imgSrc;
+        }
+    })
+
+    const boton = document.getElementById('enviarVestimenta');
+    boton.addEventListener('click' , () => {
+        const inputName = document.getElementById('nombreVestimenta').value;
+        let datos = {
+            userId: userLogin,
+            name: inputName,
+            personaje: defaultCara.src,
+            parteSuperior: defaultDorso.src,
+            pantalon: defaultPiernas.src,
+            calzado: defaultPies.src
+        }
+        console.log(datos)
+        let url = 'http://localhost:4000/characters/add';
+        fetch(url , {
+            method: 'POST',
+            body: JSON.stringify(datos),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => {
+            if(response.ok){
+                let mensajeCheck = document.getElementById('check');
+                mensajeCheck.style.display = 'block';
+                setTimeout(() => {
+                    window.location.reload();
+                },2000);
+            }
+        })
+        .catch(error => console.error(error))
+    })
+
+    const misPersonajes = document.getElementById('misPersonajes');
+    misPersonajes.addEventListener('click' , () => {
+        window.location.href = `characters.html?id=${userLogin}`;
+    })
 })
